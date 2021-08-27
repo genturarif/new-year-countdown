f6f4ef787ff0ccea213086484b4b83a409241238
genturarif <gentur1608@gmail.com>
2021-08-27 10:37:55 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const days = document.querySelectorAll("days");
+const hours = document.querySelectorAll("hours");
+const minutes = document.querySelectorAll("minutes");
+const seconds = document.querySelectorAll("seconds");
